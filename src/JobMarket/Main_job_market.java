c3b3217b6dd9092ba4d710567ff93cd41610e6f0
jjoung-2j <145014345+jjoung-2j@users.noreply.github.com>
2024-03-06 01:45:44 +0900

    [update]setting

---
+++ src/JobMarket/Main_job_market.java
@@ -0,0 +1,10 @@
+package JobMarket;
+
+public class Main_job_market {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}	// end of public static void main(String[] args)-----
+
+}
