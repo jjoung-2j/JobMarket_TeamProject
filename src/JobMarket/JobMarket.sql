3f820104f8f7807531135274ccbb2be9fc93701b
jjoung-2j <145014345+jjoung-2j@users.noreply.github.com>
2024-03-11 17:34:22 +0900

    [update]

--- src/JobMarket/JobMarket.sql
+++ src/JobMarket/JobMarket.sql
@@ -1,12 +1,2 @@
-    -- 오라클 계정 생성하기
-    -- >> SYS 연결 <<
-    
-    alter session set "_ORACLE_SCRIPT" = true;
-    
-    -- 계정명 JOB_MARKET 이고 암호는 gclass 인 사용자 계정 생성
-    create user JOB_MARKET identified by gclass default tablespace users;
-    
-    -- 권한 주기
-    grant connect, resource, create view, unlimited tablespace to JOB_MARKET;
-    
-    -- >> 
+    show user;
+    -- USER이(가) "MINI_ORAUSER1"입니다.
