085d99d470b9b942c850adb053a61d7b7a84f485
jjoung-2j <145014345+jjoung-2j@users.noreply.github.com>
2024-03-12 17:06:37 +0900

    [update]

--- src/JobMarket/Main_job_market.java
+++
@@ -1,9 +0,0 @@
-package JobMarket;
-
-public class Main_job_market {
-
-	public static void main(String[] args) {
-		System.out.println("테스트");
-	}	// end of public static void main(String[] args)-----
-
-}
