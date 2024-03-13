cffd781d91529e6c8378ab5cbeed4086960078bd
jjoung-2j <145014345+jjoung-2j@users.noreply.github.com>
2024-03-13 11:56:35 +0900

    [update] main create

---
+++ src/Controller/Controller.java
@@ -0,0 +1,13 @@
+package Controller;
+
+import java.util.Scanner;
+
+public class Controller {
+
+	// == 시작메뉴 == //
+	public void menu_Start(Scanner sc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
