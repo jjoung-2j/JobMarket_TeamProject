93ade898af1774bc85c558fbb05d7c657f03ba6a
jjoung-2j <145014345+jjoung-2j@users.noreply.github.com>
2024-03-14 14:14:22 +0900

    [update] menu_start 진행중

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Controller {
 
 	String s_Choice = "";
+	boolean isLogin = false;
+	
 	
 	// == 시작메뉴 == //
 	public void menu_Start(Scanner sc) {
@@ -15,6 +17,23 @@ public class Controller {
 		
 		System.out.print("▷ 메뉴번호 선택 : ");
 		s_Choice = sc.nextLine();
+		
+		switch (s_Choice) {
+		case "1":
+			Regiser(sc);
+			break;
+		case "2" : 
+			
+			break;
+		default:
+			break;
+		}
 	}	// end of public void menu_Start(Scanner sc)--------
+
+	// 회원가입
+	private void Regiser(Scanner sc) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
